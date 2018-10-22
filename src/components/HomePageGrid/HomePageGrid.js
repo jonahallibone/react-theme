1db7a6cef107e6ce77babe63f4efdc425d5967b8
Jonah Allibone <jonah.allibone@mymail.champlain.edu>
2018-10-22 16:48:15 -0400

    Newness

--- src/components/HomePageGrid/HomePageGrid.js
+++ src/components/HomePageGrid/HomePageGrid.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
+import ProgressiveImage from 'react-progressive-image';
 
 import './HomePageGrid.css';
 
@@ -17,24 +18,114 @@ class HomePageGrid extends Component {
             <Container className="container" fluid={true} style={{padding: 0}}>
                 <div className="grid-container">
                     <div className="grid-container--side of-1">
-                        <div className="item"></div>
+                        <div className="item">
+                            <ProgressiveImage src="https://s3.amazonaws.com/piscatello/DesignAtWork_Horiz.jpg" placeholder="tiny-image.jpg">
+                                {(src, loading) => (
+                                    <div className={ loading ? "img-container" : "img-container loaded" }>
+                                        <img src={src} alt="an image" />
+                                    </div>
+                                )}
+                            </ProgressiveImage>
+                            <h3>Design At Work Book</h3>
+                        </div>
                     </div>
                     <div className="grid-container--side of-4">
-                        <div className="item"></div>
-                        <div className="item"></div>
-                        <div className="item"></div>
-                        <div className="item"></div>
+                        <div className="item">
+                            <ProgressiveImage src="https://s3.amazonaws.com/piscatello/AEA_EquityWorks.jpg" placeholder="tiny-image.jpg">
+                                {(src, loading) => (
+                                    <div className={ loading ? "img-container" : "img-container loaded" }>
+                                        <img src={src} alt="an image" />
+                                    </div>
+                                )}
+                            </ProgressiveImage>
+                            <h3>AEA Equality Works</h3>
+                        </div>
+                        <div className="item">
+                            <ProgressiveImage src="https://s3.amazonaws.com/piscatello/CoffeeHouse_Banner.jpg" placeholder="tiny-image.jpg">
+                                {(src, loading) => (
+                                    <div className={ loading ? "img-container" : "img-container loaded" }>
+                                        <img src={src} alt="an image" />
+                                    </div>
+                                )}
+                            </ProgressiveImage>
+                            <h3>Coffee House Banner</h3>
+                        </div>
+                        <div className="item">
+                            <ProgressiveImage src="https://s3.amazonaws.com/piscatello/Lost-Tribe_Rainbow.jpg" placeholder="tiny-image.jpg">
+                                {(src, loading) => (
+                                    <div className={ loading ? "img-container" : "img-container loaded" }>
+                                        <img src={src} alt="an image" />
+                                    </div>
+                                )}
+                            </ProgressiveImage>
+                            <h3>LostTribe Theatre Company</h3>
+                        </div>
+                        <div className="item">
+                            <ProgressiveImage src="https://s3.amazonaws.com/piscatello/DW06.jpg" placeholder="tiny-image.jpg">
+                                {(src, loading) => (
+                                    <div className={ loading ? "img-container" : "img-container loaded" }>
+                                        <img src={src} alt="an image" />
+                                    </div>
+                                )}
+                            </ProgressiveImage>
+                            <h3>Douglass Winthrop</h3>
+                        </div>
                     </div>
                 </div>
                 <div className="grid-container">
                     <div className="grid-container--side of-4">
-                        <div className="item"></div>
-                        <div className="item"></div>
-                        <div className="item"></div>
-                        <div className="item"></div>
+                        <div className="item">
+                                <ProgressiveImage src="https://s3.amazonaws.com/piscatello/Lost-Tribe_Bus.jpg" placeholder="tiny-image.jpg">
+                                    {(src, loading) => (
+                                        <div className={ loading ? "img-container" : "img-container loaded" }>
+                                            <img src={src} alt="an image" />
+                                        </div>
+                                    )}
+                                </ProgressiveImage>
+                            <h3>LostTribe Theatre Group</h3>
+                        </div>
+                        <div className="item">
+                            <ProgressiveImage src="https://s3.amazonaws.com/piscatello/Nomadx-BC.jpg" placeholder="tiny-image.jpg">
+                                {(src, loading) => (
+                                    <div className={ loading ? "img-container" : "img-container loaded" }>
+                                        <img src={src} alt="an image" />
+                                    </div>
+                                )}
+                            </ProgressiveImage>
+                            <h3>Nomadx</h3>
+                        </div>
+                        <div className="item">
+                            <ProgressiveImage src="https://s3.amazonaws.com/piscatello/Riverside_Entrance.jpg" placeholder="tiny-image.jpg">
+                                {(src, loading) => (
+                                    <div className={ loading ? "img-container" : "img-container loaded" }>
+                                        <img src={src} alt="an image" />
+                                    </div>
+                                )}
+                            </ProgressiveImage>
+                            <h3>Riverside Entrance</h3>
+                        </div>
+                        <div className="item">
+                                <ProgressiveImage src="https://s3.amazonaws.com/piscatello/Salt-Lake_Kiosks.jpg" placeholder="tiny-image.jpg">
+                                    {(src, loading) => (
+                                        <div className={ loading ? "img-container" : "img-container loaded" }>
+                                            <img src={src} alt="an image" />
+                                        </div>
+                                    )}
+                                </ProgressiveImage>
+                            <h3>The US Courthouse</h3>
+                        </div>
                     </div>
                     <div className="grid-container--side of-1">
-                        <div className="item"></div>
+                        <div className="item">
+                            <ProgressiveImage src="https://s3.amazonaws.com/piscatello/Timeless_Entrance.jpg" placeholder="tiny-image.jpg">
+                                {(src, loading) => (
+                                    <div className={ loading ? "img-container" : "img-container loaded" }>
+                                        <img src={src} alt="an image" />
+                                    </div>
+                                )}
+                            </ProgressiveImage>
+                            <h3>Timeless Massimo Vignelli</h3>
+                        </div>
                     </div>
                 </div>
             </Container>
