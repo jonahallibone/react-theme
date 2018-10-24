f003d72f965397fd5ba9a9e757a0b68cd7a6e600
jonahallibone <jonah.allibone@mymail.champlain.edu>
2018-10-24 14:46:47 -0400

    New addition

--- src/components/ContactBand/ContactBand.js
+++ src/components/ContactBand/ContactBand.js
@@ -10,15 +10,16 @@ function ContactBand() {
         <div id="contact-band" className="padding-top-6 padding-btm-6">
             <Container className="container" fluid={true} style={{padding: "0"}}>
                 <Row>
-                    <Col xs={12} sm={6}>
-                        <h2 className="reg margin-btm-3">Lets work together.</h2>
+                    <Col xs={12} sm={8}>
+                        <h2 className="reg margin-btm-3">The experience is your brand.</h2>
                         <h3 className="reg light margin-btm-3">
-                            Our work changes the way businesses think about
-                            themselves and operate. We create value for our 
-                            clients and positive experiences for people and society. 
-                            Contact us to talk about working together.
+                        Our work encompasses business strategy, brand identity, 
+                        environmental signage, and website development. We 
+                        view systems in its entirety and always from a user 
+                        perspective. Design, when implemented consistently adds 
+                        brand value and reduces operational costs.
                         </h3>
-                        <Button button-lg className="margin-btm-3">Get in touch</Button>
+                        <Button button-lg className="margin-btm-3">Request a meeting</Button>
                     </Col>
                 </Row>
             </Container>
