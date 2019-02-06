22cc51bd92ae9b35e6980021fd710d08b2c70fae
Jonah Allibone <jonah.allibone@mymail.champlain.edu>
2019-02-06 18:05:14 -0500

    Added animations to the projects

--- src/components/Careers/Careers.js
+++ src/components/Careers/Careers.js
@@ -3,66 +3,77 @@ import { Container, Row, Col } from 'react-grid-system';
 import HoverLink from '../HoverLink/HoverLink';
 
 import './Careers.css';
-import CareerPageSingle from '../CareerPageSingle/CareerPageSingle';
 
 class Careers extends Component {
+    componentDidMount() {
+        this.styleBlack();
+    }
+
+    styleBlack = () => {
+        document.querySelector("header").classList.remove("white-bg");
+        document.querySelector("header").classList.add("black-bg");
+    }
+    
     render() {
         return (
-            <div id="careers">
+            <div className="careers-page">
                 <Container fluid={true} className="container" style={{padding: 0}}>
-                    <h3 className="reg text-bold padding-top-4 text-white text-capitalize">
-                        Careers at Piscatello Design Centre
-                    </h3>
-                    <Row>
-                        <Col xs={12} sm={10} md={8} lg={7}  style={{ paddingBottom: "5rem"}}>
-                            <h3 className="reg text-grey light padding-top-2">
-                                Join our team of designers and creators.
-                            </h3>
-                        </Col>
-                    </Row>
+                    <h1 className="text-white lighter reg careers-top-text">We're always on the lookout for the best designers, strategists, and developers to join our team.</h1>
+                    <div className="image-container">
+                        <img src="https://s3.amazonaws.com/piscatello/studio-hands.jpg" />
+                    </div>
+                    <div className="careers-grid career">
+                        <div>
+                            <h2 className="reg text-white text-bold">Internships</h2>
+                            <h3 className="reg text-grey light">New York, New York</h3>
+                        </div>
+                        <div>
+                            <p className="text-grey">
+                                We are currently accepting applications for design, strategy, and development internships. Experience in the relevant discipline is an advantage. Commitment, passion, flexibility, and independence is essential.
+                            </p><br/>
+                            <p className="text-grey">
+                                Interns work closely with our team on varied client and internal projects. A solid understanding of Piscatello Design Centre’s work and philosophy is vital.
+                            </p><br/>
+                            <p className="text-grey">
+                                Please send your CV, and/or pdf portfolio, and cover letter, as well as your availability to internships@piscatello.com
+                            </p>
+                        </div>
+                    </div>
+                    <div className="careers-grid career">
+                        <div>
+                            <h2 className="reg text-white text-bold">Internships</h2>
+                            <h3 className="reg text-grey light">New York, New York</h3>
+                        </div>
+                        <div>
+                            <p className="text-grey">
+                                We are currently accepting applications for design, strategy, and development internships. Experience in the relevant discipline is an advantage. Commitment, passion, flexibility, and independence is essential.
+                            </p><br/>
+                            <p className="text-grey">
+                                Interns work closely with our team on varied client and internal projects. A solid understanding of Piscatello Design Centre’s work and philosophy is vital.
+                            </p><br/>
+                            <p className="text-grey">
+                                Please send your CV, and/or pdf portfolio, and cover letter, as well as your availability to internships@piscatello.com
+                            </p>
+                        </div>
+                    </div>
+                    <div className="careers-grid career">
+                        <div>
+                            <h2 className="reg text-white text-bold">Internships</h2>
+                            <h3 className="reg text-grey light">New York, New York</h3>
+                        </div>
+                        <div>
+                            <p className="text-grey">
+                                We are currently accepting applications for design, strategy, and development internships. Experience in the relevant discipline is an advantage. Commitment, passion, flexibility, and independence is essential.
+                            </p><br/>
+                            <p className="text-grey">
+                                Interns work closely with our team on varied client and internal projects. A solid understanding of Piscatello Design Centre’s work and philosophy is vital.
+                            </p><br/>
+                            <p className="text-grey">
+                                Please send your CV, and/or pdf portfolio, and cover letter, as well as your availability to internships@piscatello.com
+                            </p>
+                        </div>
+                    </div>
                 </Container>
-                <section style={{background: "#FFF"}}>
-                    <Container fluid={true} className="container" style={{padding: 0}}>
-                        <div className="image-grid">
-                            <div className="img-container">
-                                <img src="http://piscatello.com/wp-content/themes/pdc/images/studio_light.jpg" alt=""/>
-                            </div>
-                            <div className="img-container">
-                                <img src="http://piscatello.com/wp-content/themes/pdc/images/studio_people.jpg" alt=""/>
-                            </div>
-                        </div>
-                        <div className="career-grid">
-                            <div className="career-block">
-                                <h3 className="reg">Graphic Design Intern</h3>
-                                <p className="text-grey margin-top-1">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus faucibus 
-                                nunc ut bibendum. Sed pretium varius orci, quis egestas enim aliquet at. Maecenas 
-                                pellentesque molestie velit vitae tincidunt. Sed finibus pretium lorem, congue 
-                                malesuada mi imperdiet ac. 
-                                </p>
-                                
-                            </div>
-                            <div className="career-block">
-                                <h3 className="reg">Full Stack Engineer</h3>
-                                <p className="text-grey margin-top-1">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus faucibus 
-                                nunc ut bibendum. Sed pretium varius orci, quis egestas enim aliquet at. Maecenas 
-                                pellentesque molestie velit vitae tincidunt. Sed finibus pretium lorem, congue 
-                                malesuada mi imperdiet ac. 
-                                </p>
-                            </div>
-                            <div className="career-block">
-                                <h3 className="reg">Graphic Designer</h3>
-                                <p className="text-grey margin-top-1">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus faucibus 
-                                nunc ut bibendum. Sed pretium varius orci, quis egestas enim aliquet at. Maecenas 
-                                pellentesque molestie velit vitae tincidunt. Sed finibus pretium lorem, congue 
-                                malesuada mi imperdiet ac. 
-                                </p>
-                            </div>
-                        </div>
-                    </Container>
-                </section>
             </div>
         )
     }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,13 +14,13 @@ class Footer extends Component {
                             <a className="light pdc-copy">
                                Piscatello Design Centre, 1997 - {(new Date()).getFullYear()}
                             </a>
-                            <a className="light"><HoverLink grey-black="true">Stay Updated</HoverLink></a>
+                            <Link className="light" to="/careers"><HoverLink grey-black="true">Careers</HoverLink></Link>
+                            <Link className="light" to="/update"><HoverLink grey-black="true">Stay Updated</HoverLink></Link>
                         </div>
                         <div className="column-right break">
                             <a className="light"><HoverLink grey-black="true">Instagram</HoverLink></a>
                             <a className="light"><HoverLink grey-black="true">LinkedIn</HoverLink></a>
                             <a className="light"><HoverLink grey-black="true">Twitter</HoverLink></a>
-                            {/* <a className="light"><HoverLink grey-black="true">Careers</HoverLink></a> */}
                             <Link className="light" to="/contact"><HoverLink grey-black="true">Contact</HoverLink></Link>
                         </div>
                     </div>
