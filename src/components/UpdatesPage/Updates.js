00403616dfac4ec746b2cd085ad0b69fd8542230
Jonah Allibone <jonah.allibone@mymail.champlain.edu>
2019-02-27 15:35:55 -0500

    Added in update page linking to API

--- src/components/UpdatesPage/Updates.js
+++ src/components/UpdatesPage/Updates.js
@@ -64,7 +64,7 @@ class Updates extends Component {
                     </div>
                     <div className="update-des">
                         <h3 className="reg text-bold">
-                            <HoverLink>{el.title.rendered}</HoverLink>
+                            <HoverLink black={true}>{el.title.rendered}</HoverLink>
                         </h3>
                         <p className="text-grey light padding-top-175">{new Date(el.date).toLocaleDateString('en-US', options)}</p>
                         <br/>
