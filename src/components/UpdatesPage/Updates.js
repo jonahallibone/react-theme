a48bd2bd3d90bb8df710bda6c188145e46bb3fcc
Jonah Allibone <jonah.allibone@mymail.champlain.edu>
2019-04-26 15:33:47 -0400

    Dev push

--- src/components/UpdatesPage/Updates.js
+++ src/components/UpdatesPage/Updates.js
@@ -76,20 +76,19 @@ class Updates extends Component {
         return updates.map(el => {
             return (
                 <Link to={`/update/${el.slug}`} className="update-row" key={el.id}>
+                    <div className="update-img">
+                        <img src={el.acf.featured_image} alt={el.title.rendered}/>
+                    </div>
                     <div className="update-des">
                         <div>
-                            <h3 className="reg text-bold">
-                                <HoverLink grey-black={true}>{el.title.rendered}</HoverLink>
-                            </h3>
                             <p className="text-grey light">{this.getDateFormat(el.date)}</p>
+                            <p className="reg text-bold text-white">
+                                <HoverLink>{el.title.rendered}</HoverLink>
+                            </p>
                         </div>
                         <div>
                             <p className="text-grey light" dangerouslySetInnerHTML={{__html: el.excerpt.rendered}} />
-                            <p><HoverLink grey-black={true}>Read More</HoverLink></p>
                         </div>
-                    </div>
-                    <div className="update-img">
-                        <img src={el.acf.featured_image} alt={el.title.rendered}/>
                     </div>
                 </Link>
             )
@@ -101,7 +100,7 @@ class Updates extends Component {
             <section className="updates-page" style={{paddingTop: "70px"}}>
                 <Container fluid={true} className="container" style={{padding: 0}}>
                     <div className="updates-heading">
-                        <h1 className="text-black reg lighter">Stay Updated.</h1> 
+                        <h1 className="text-white reg lighter">Stay Updated</h1> 
                         <div className="loader"></div>
                         <div className="thank-you-text">
                             <h1 className="text-grey reg lighter">Thank you for subscribing.</h1>
@@ -119,42 +118,6 @@ class Updates extends Component {
                 <section className="update-section">
                     <Container fluid={true} className="container" style={{padding: 0, display: "flex", alignItems: "center"}}>
                         <div className="update-feed">
-                            <div style={{bottom: "0px"}} className="news-column">
-                                <div className="reg text-grey light hover-underline news-side-item padding-btm-md">
-                                    <h3 className="text-black reg"><span className="text-black text-bold">03 April</span></h3>
-                                    <p className="padding-top-1"><span className="text-grey text-light">Work</span></p>
-                                    <p className="text-black">
-                                        Piscatello Design Centre launches a new website, redesigned and developed from the ground up.
-                                    </p>
-                                </div>
-                                <div className="reg text-grey light hover-underline news-side-item padding-btm-md">
-                                    <a href="http://www.cadc.org/announcements/4096884/reflections-on-ten-qualities-of-design-11-15" target="_blank">
-                                        <h3 className="text-black reg"><span className="text-black text-bold">23 February</span></h3>
-                                        <p className="padding-top-1"><span className="text-grey text-light">Exhibition</span></p>
-                                        <p className="text-red">
-                                            Reflections on Ten Qualities of Design Exhibition at the University of New Haven lengthened
-                                            <span className="link-arrow black"></span>
-                                        </p>
-                                    </a>
-                                </div>
-                                <div className="reg text-grey light hover-underline news-side-item padding-btm-md">
-                                    <h3 className="text-black reg"><span className="text-black text-bold">12 November</span></h3>
-                                    <p className="padding-top-1"><span className="text-grey text-light">Lecture</span></p>
-                                    <p className="text-black">
-                                        Rocco Piscatello to Speak at the University of New Haven
-                                    </p>
-                                </div>    
-                                <div className="reg text-grey light hover-underline news-side-item padding-btm-md">
-                                    <a href="https://www.mohawkconnects.com/article/mohawk-blog/rocco-piscatello-poster-works-fit" target="_blank">
-                                        <h3 className="text-black reg"><span className="text-black text-bold">05 November</span></h3>
-                                        <p className="padding-top-1"><span className="text-grey text-light">Featured</span></p>
-                                        <p className="text-red">
-                                            Mohawk Fine Paper Features Rocco Piscatello’s Ten Qualities of Design
-                                            <span className="link-arrow black"></span>
-                                        </p>
-                                    </a>
-                                </div>                          
-                            </div>
                             <ProjectsContext.Consumer>
                                 {({ updates }) => (
                                     <div className="update-feed--grid">
@@ -162,8 +125,6 @@ class Updates extends Component {
                                     </div>
                                 )}
                             </ProjectsContext.Consumer>
-                            
-                            
                         </div>
                 </Container>
             </section>
